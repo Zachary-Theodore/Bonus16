90f2beafc9c9781e25fa08cdea7fc36a192224e7
Zachary_Theodore <fk9836@wayne.edu>
2018-07-30 15:04:39 -0400

    Intial Commit

---
+++ Bonus16/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus16
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        public string Make
+        {
+            set { make = value; }
+            get { return make; }
+        }
+
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+
+        public int Year
+        {
+            set { year = value; }
+            get { return year; }
+        }
+
+        public double Price
+        {
+            set { price = value; }
+            get { return price; }
+        }
+    }
+}
---
+++ Bonus16/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            while (true)
+            {
+                Console.Clear();
+                Car temp = new Car();
+                Console.WriteLine("Please Provide a Make:");
+                temp.Make = (Console.ReadLine());
+                Console.WriteLine("Please Provide a Model:");
+                temp.Model = (Console.ReadLine());
+                Console.WriteLine("Please Provide a Year:");
+                temp.Year = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please Provide a Price:");
+                temp.Price = double.Parse(Console.ReadLine());
+
+                cars.Add(temp);
+
+                Console.WriteLine("Add another Vehicle?");
+                string choice = Console.ReadLine();
+                if (choice == "N")
+                {
+                    break;
+                }
+            }
+
+            foreach (Car e in cars)
+            {
+                Console.WriteLine($"{e.Make}, {e.Model}, {e.Year}, ${e.Price}");
+                Console.WriteLine("========================================");
+            }
+
+
+
+
+
+
+            
+        }
+    }
+}
